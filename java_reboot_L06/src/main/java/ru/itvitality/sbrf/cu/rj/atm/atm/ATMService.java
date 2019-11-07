2b71154aeac4e8062bd95d1210b32734a55d636d
1121977 <57187986+1121977@users.noreply.github.com>
2019-11-08 00:06:01 +0300

    Delete ATMService.java
    sss

--- java_reboot_L06/src/main/java/ru/itvitality/sbrf/cu/rj/atm/atm/ATMService.java
+++
@@ -1,8 +0,0 @@
-package ru.itvitality.sbrf.cu.rj.atm.atm;
-
-import java.io.IOException;
-
-public interface ATMService {
-    Integer getBalance();
-    void saveToFile(String fileName) throws IOException;
-}
